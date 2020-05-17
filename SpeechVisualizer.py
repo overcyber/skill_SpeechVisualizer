f62b9d1af4424e5c91d69218a74d9dcbaedcefc3
Philipp <41761223+philipp2310@users.noreply.github.com>
2020-05-18 00:08:15 +0200

    workingcopy 0.4

--- SpeechVisualizer.py
+++ SpeechVisualizer.py
@@ -5,3 +5,11 @@ class SpeechVisualizer(AliceSkill):
 
 	def __init__(self):
 		super().__init__()
+
+
+		if not lastScore \
+				or biggerIsBetter and score > int(lastScore['score']) \
+				or not biggerIsBetter and score < int(lastScore['score']):
+			return True
+		else:
+			return False
